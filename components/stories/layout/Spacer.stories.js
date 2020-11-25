6b153bd261d0f9418f2763faa4e332bbc4382848
Brando Vidal <yisusdracon@gmail.com>
2020-11-25 02:20:08 -0500

    [FIX] Update Spacer and tokens
    Signed-off-by: Brando Vidal <yisusdracon@gmail.com>

--- components/stories/layout/Spacer.stories.js
+++ components/stories/layout/Spacer.stories.js
@@ -6,7 +6,12 @@ export default {
   component: Spacer,
 }
 
-export const Default = () => <Spacer size={8} isVisible={true}></Spacer>
+export const Zero = () => <Spacer size="0" isVisible={true}></Spacer>
+export const One = () => <Spacer size="px" isVisible={true}></Spacer>
+export const extraSmall = () => <Spacer size="xs" isVisible={true}></Spacer>
+export const Small = () => <Spacer size="sm" isVisible={true}></Spacer>
+export const Medium = () => <Spacer size="md" isVisible={true}></Spacer>
+export const Large = () => <Spacer size="lg" isVisible={true}></Spacer>
 export const Horizontal = () => (
   <Spacer.Horizontal size={8} isVisible={true}></Spacer.Horizontal>
 )
